506facb96e84d40a21e2877f99de26b2bad51360
stathis-st <stathis.stamatis@gmail.com>
2022-10-29 19:40:20 +0300

    Initial commit

---
+++ src/main/java/com/stathis/cardcostapi/CardCostApiApplication.java
@@ -0,0 +1,13 @@
+package com.stathis.cardcostapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardCostApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardCostApiApplication.class, args);
+    }
+
+}
