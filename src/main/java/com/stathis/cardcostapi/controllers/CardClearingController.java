e20a895d8278ea731cde5d743b600e4da99b1076
stathis-st <stathis.stamatis@gmail.com>
2022-10-30 13:51:26 +0200

    Add CardClearing controller class

---
+++ src/main/java/com/stathis/cardcostapi/controllers/CardClearingController.java
@@ -0,0 +1,45 @@
+package com.stathis.cardcostapi.controllers;
+
+import com.stathis.cardcostapi.domain.CardClearing;
+import com.stathis.cardcostapi.services.CardClearingService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(CardClearingController.BASE_URL)
+@AllArgsConstructor
+public class CardClearingController {
+
+    public static final String BASE_URL = "api/v1/card-clearings";
+
+    private final CardClearingService cardClearingService;
+
+    @GetMapping
+    public List<CardClearing> getAllCardClearings() {
+        return cardClearingService.getAllCardClearings();
+    }
+
+    @GetMapping("/{id}")
+    public CardClearing getCardClearingById(@PathVariable("id") Long id) {
+        return cardClearingService.getCardClearingById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CardClearing saveCardClearing(@RequestBody CardClearing cardClearing) {
+        return cardClearingService.saveCardClearing(cardClearing);
+    }
+
+    @PutMapping("/{id}")
+    public CardClearing updateCardClearing(@PathVariable("id") Long id, @RequestBody CardClearing cardClearing) {
+        return cardClearingService.updateCardClearing(id, cardClearing);
+    }
+
+    @DeleteMapping
+    public void deleteCardClearing(@PathVariable("id") Long id) {
+        cardClearingService.deleteCardClearing(id);
+    }
+}
