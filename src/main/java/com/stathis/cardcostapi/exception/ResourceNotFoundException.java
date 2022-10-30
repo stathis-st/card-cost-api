231b8a57e970853885f7375b35a987518b0b1853
stathis-st <stathis.stamatis@gmail.com>
2022-10-30 17:48:22 +0200

    Add custom exception classes

---
+++ src/main/java/com/stathis/cardcostapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.stathis.cardcostapi.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public static final String RESOURCE_NOT_FOUND_WITH_ID = "Resource not found with id = ";
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/stathis/cardcostapi/exception/ResourceNotUpdatedException.java
@@ -0,0 +1,10 @@
+package com.stathis.cardcostapi.exception;
+
+public class ResourceNotUpdatedException extends RuntimeException {
+
+    public static final String RESOURCE_COULD_NOT_BE_UPDATED = "Resource could not be updated: ";
+
+    public ResourceNotUpdatedException(String message) {
+        super(message);
+    }
+}
