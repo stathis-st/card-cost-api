991651835a30e13284535daef5f47307754101f9
stathis-st <stathis.stamatis@gmail.com>
2022-10-30 20:29:36 +0200

    Refactor RestResponseEntityExceptionHandler class in order to use ApiError class

--- src/main/java/com/stathis/cardcostapi/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/stathis/cardcostapi/controllers/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.stathis.cardcostapi.controllers;
 
+import com.stathis.cardcostapi.error.ApiError;
 import com.stathis.cardcostapi.exception.ResourceConstraintViolationException;
 import com.stathis.cardcostapi.exception.ResourceNotDeletedException;
 import com.stathis.cardcostapi.exception.ResourceNotFoundException;
@@ -9,21 +10,33 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ResourceNotFoundException.class,
             ResourceNotDeletedException.class})
-    public ResponseEntity<Object> handleNotFoundException(Exception exception, WebRequest request) {
-        return new ResponseEntity<>(exception.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<Object> handleNotFoundException(Exception exception, HttpServletRequest request) {
+        return new ResponseEntity<>(buildApiError(exception, request, HttpStatus.NOT_FOUND), new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({ResourceConstraintViolationException.class,
             ResourceNotUpdatedException.class})
-    public ResponseEntity<Object> handleBadRequest(Exception exception, WebRequest request) {
-        return new ResponseEntity<>(exception.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> handleBadRequest(Exception exception, HttpServletRequest request) {
+        return new ResponseEntity<>(buildApiError(exception, request, HttpStatus.BAD_REQUEST), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    private ApiError buildApiError(Exception exception, HttpServletRequest request, HttpStatus httpStatus) {
+        return ApiError.builder()
+                .timestamp(LocalDateTime.now())
+                .status(httpStatus.value())
+                .error(httpStatus.getReasonPhrase())
+                .message(exception.getMessage())
+                .path(request.getRequestURI())
+                .build();
     }
 }
