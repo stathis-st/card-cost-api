862734e01ae75c88bb29a6eaa023144aa882d735
stathis-st <stathis.stamatis@gmail.com>
2022-10-30 13:37:31 +0200

    Add CardClearing service implementation class

---
+++ src/main/java/com/stathis/cardcostapi/services/CardClearingServiceImpl.java
@@ -0,0 +1,53 @@
+package com.stathis.cardcostapi.services;
+
+import com.stathis.cardcostapi.domain.CardClearing;
+import com.stathis.cardcostapi.repositories.CardClearingRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CardClearingServiceImpl implements CardClearingService {
+
+    private final CardClearingRepository cardClearingRepository;
+
+    @Override
+    public List<CardClearing> getAllCardClearings() {
+        return cardClearingRepository.findAll();
+    }
+
+    @Override
+    public CardClearing getCardClearingById(Long id) {
+        return cardClearingRepository.findById(id).orElseThrow(() -> new RuntimeException("Resource with " + id + " not found"));
+    }
+
+    @Override
+    public CardClearing saveCardClearing(CardClearing cardClearing) {
+        return cardClearingRepository.save(cardClearing);
+    }
+
+    @Override
+    public CardClearing updateCardClearing(Long id, CardClearing cardClearing) {
+        CardClearing savedCardClearing;
+        try {
+            savedCardClearing = getCardClearingById(id);
+        } catch (RuntimeException ex) {
+            throw new RuntimeException("Resource could not be updated: " + ex.getMessage());
+        }
+        savedCardClearing.setCountryCode(cardClearing.getCountryCode());
+        savedCardClearing.setClearingCost(cardClearing.getClearingCost());
+        return saveCardClearing(savedCardClearing);
+    }
+
+    @Override
+    public void deleteCardClearing(Long id) {
+        try {
+            cardClearingRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException ex) {
+            throw new RuntimeException("There is no resource to be deleted with id = " + id);
+        }
+    }
+}
