c0bbe8c8a94c1d6f4f8b83620443c0d35dd4229e
stathis-st <stathis.stamatis@gmail.com>
2022-11-01 01:08:49 +0200

    Add method in CardClearingRepository in order to query by country code

--- src/main/java/com/stathis/cardcostapi/repositories/CardClearingRepository.java
+++ src/main/java/com/stathis/cardcostapi/repositories/CardClearingRepository.java
@@ -4,4 +4,5 @@ import com.stathis.cardcostapi.domain.CardClearing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardClearingRepository extends JpaRepository<CardClearing, Long> {
+    CardClearing findCardClearingByCountryCode(String countryCode);
 }
